e3ba88cb823fa5c3aa304fa2bfd7df02524e4b91
Sina Chavoshi <chavoshi@google.com>
2019-08-07 11:40:02 -0700

    Adding script based build file

--- component_build.py
+++ component_build.py
@@ -1,3 +1,25 @@
+"""TODO(chavoshi): DO NOT SUBMIT without one-line documentation for component_build.
+
+TODO(chavoshi): DO NOT SUBMIT without a detailed description of component_build.
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import google_type_annotations
+from __future__ import print_function
+
+from absl import app
+from absl import flags
+
+FLAGS = flags.FLAGS
+
+
+def main(argv):
+  if len(argv) > 1:
+    raise app.UsageError('Too many command-line arguments.')
+
+if __name__ == '__main__':
+  app.run(main)
 #!/usr/bin/env python3
 # Copyright 2019 Google LLC
 #
